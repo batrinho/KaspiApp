9eedda5bcc3176af2fab28e4975919f832b5e135
Batyr Tolkynbayev <batyrtolkynbayev@gmail.com>
2024-01-16 18:07:04 +0600

    Initial Commit

---
+++ KaspiApp/KaspiAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  KaspiAppApp.swift
+//  KaspiApp
+//
+//  Created by Batyr Tolkynbayev on 16.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct KaspiAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
